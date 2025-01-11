34f24e0f04f2d0d138f6090a8b8e947aad3f1046
sramirez124 <forwardemails124@gmail.com>
2025-01-11 14:25:10 -0600

    Added a check for one section

---
+++ 1400. Construct K Palindrome Strings/1400-Palindrome-Strings.js
@@ -0,0 +1,14 @@
+/**
+ * Given a string s and an integer k, return true if you can use all the 
+ * characters in s to construct k palindrome strings or false otherwise.
+ */
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {boolean}
+ */
+var canConstruct = function(s, k) {
+    if (s.length < k) return false;
+    
+};
