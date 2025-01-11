647564b55c98a8f569871df04b9efc7a23db043a
sramirez124 <forwardemails124@gmail.com>
2025-01-11 12:40:32 -0600

    Added in the instructions for 2620. Counter

--- 2620. Counter/Counter.js
+++ 2620. Counter/Counter.js
@@ -1,5 +1,7 @@
 /**
  * 
+https://leetcode.com/problems/counter/description/
+
 Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 Example 1:
 
@@ -30,3 +32,23 @@ Constraints:
 
 
  */
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    return function() {
+        return n++;
+    };
+};
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
+
+
+// 01/11/2025
