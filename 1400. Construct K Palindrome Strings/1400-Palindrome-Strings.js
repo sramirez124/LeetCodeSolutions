455d5ae3a8f49a7c911f1a043d73cd5c88ec1527
sramirez124 <forwardemails124@gmail.com>
2025-01-20 15:08:29 -0600

    Update 1400-Palindrome-Strings.js

--- 1400. Construct K Palindrome Strings/1400-Palindrome-Strings.js
+++ 1400. Construct K Palindrome Strings/1400-Palindrome-Strings.js
@@ -13,15 +13,15 @@ var canConstruct = function(s, k) {
     
     let countArr = Array(26).fill(0);
     for (const char of s) {
-        count[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+        countArr[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
     }
 
     let odd = 0;
     for (const count of countArr) {
-        if (count % 2 !== 0) odd++;
-        if (odd > 1) return false;
+        if (count % 2 !== 0) 
+            odd++;
     }
     
-    return true;
+    return odd <= k;
     
 };
